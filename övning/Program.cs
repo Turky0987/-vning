102934963fe7a7a3a2d2efab89185e775be89d89
taal04003 <taal04003@E-PW0234KH>
2023-09-06 10:35:22 +0200

    Add project files.

---
+++ övning/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Detta är ett program gjort i c#");
+Console.WriteLine("Programmet skriver ut text på flera rader.");
+Console.WriteLine("Detta är programmets sista utskrift.");
+
+
